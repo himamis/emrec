e9a0cfabdf5f84eb261604216e954efbd02fd24d
Balazs Bencze <megette_a@yahoo.com>
2014-07-17 13:48:02 +0300

    DatasetKey interface added

---
+++ common/src/main/java/edu/ubbcluj/emotion/model/DatasetKey.java
@@ -0,0 +1,13 @@
+package edu.ubbcluj.emotion.model;
+
+/**
+ * Tagging interface.
+ * <p>
+ * Classes implementing this interface should ensure a safe implementation of the {@link Object#equals(Object) equals} and {@link Object#hashCode() hashCode} methods.
+ * </p>
+ * @author bencze
+ *
+ */
+public interface DatasetKey {
+
+}
