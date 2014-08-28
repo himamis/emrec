e2b7f7408cf750c4212da40067b60317693bd764
Balazs Bencze <megette_a@yahoo.com>
2014-08-28 21:39:27 +0200

    [dbeditor] comments added

--- dbeditor/src/edu/ubbcluj/emotion/editor/SequenceEditor.java
+++ dbeditor/src/edu/ubbcluj/emotion/editor/SequenceEditor.java
@@ -6,12 +6,31 @@ import edu.ubbcluj.emotion.model.Sequence;
 
 public interface SequenceEditor {
 	
+	/**
+	 * This method will be called before editing a sequence.
+	 * 
+	 * @param sequence the sequence to edit
+	 */
 	public void setUp(Sequence sequence);
 
+	/**
+	 * Applies the transformation to the given image. Landmarks 
+	 * can be null, if {@link SequenceEditor#inputsLandmarks()
+	 * inputsLandmarks()} returns false.
+	 * 
+	 * @param image the image to edit
+	 * @param landmarks the landmarks associated with the image
+	 */
 	public void doEdit(Image image, Landmarks landmarks);
 
+	/**
+	 * Whether the class transforms the landmarks.
+	 */
 	public boolean outputsLandmarks();
 	
+	/**
+	 * Whether the class needs landmarks to edit the image.
+	 */
 	public boolean inputsLandmarks();
 
 }
